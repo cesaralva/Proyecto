f24556193de70c6c259598d96d098cf9ef08f456
CESAR ENRIQUE ALVA VALDIVIA <1191054@senati.pe>
2022-02-22 21:35:02 -0500

    master

--- pruebas.php
+++ pruebas.php
@@ -23,6 +23,7 @@ for($n = 1; $n <= count($ArrayA); $n++):
 <a href="<?php if ($miArray[$i] == $miArray[5]) { echo $miArray[5]; } else if ($miArray[$i] == $miArray[$n-1]) { echo $miArray[$i-1]; } ?>">
     <img src="<?php echo $miArray[$i]; ?>">
 </a>
+ 
 
 <?php
 endfor;
